a7cb17e5b61902490a4578e2b60f8eb286bf1032
martagrio <marta.igr90@gmail.com>
2019-08-08 13:42:37 +0200

    Add listener for fave and basic CSS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,7 +6,7 @@ const list = document.querySelector('.series__list');
 const input = document.querySelector('.input--serie');
 const button = document.querySelector('.btn');
 
-const getName = () => {
+function getName() {
   const name = input.value.replace(/\s+/gi, '-'); //to put - instead of spaces
   const endpoint = urlBase + name;
   list.innerHTML = '';
@@ -14,7 +14,7 @@ const getName = () => {
   fetch(endpoint)
     .then(response => response.json())
     .then(data => getShow(data));
-};
+}
 
 //Function to add a default photo if there is no one in the array for the show
 function getImage(array, i) {
@@ -30,8 +30,11 @@ function getShow (array){
 
   for (let i = 0; i < array.length; i++) {
     const show = document.createElement('li');
+    show.classList.add('show--li');
     const image = document.createElement('img');
+    image.classList.add('show--image');
     const title = document.createElement('h2');
+    title.classList.add('show--title');
     const titleContent = document.createTextNode(array[i].show.name);
     title.appendChild(titleContent);
     show.appendChild(title);
@@ -40,6 +43,19 @@ function getShow (array){
     image.alt = array[i].show.name;
     show.appendChild(image);
     list.appendChild(show);
+  }
+
+  //Function to click on any li and got it selected as fave
+
+  const showLi = document.querySelectorAll('.show--li');
+
+  function markFave (event) {
+    const currentLi = event.currentTarget;
+    currentLi.classList.toggle('faveShow');
+
+  }
+  for (const item of showLi) {
+    item.addEventListener('click', markFave);
   }
 }
 
@@ -53,3 +69,4 @@ function preshEnter(event) {
 }
 
 input.addEventListener('keydown', preshEnter);
+
