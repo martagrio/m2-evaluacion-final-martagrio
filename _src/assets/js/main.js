559d6b7dd93d286d89d1befa7783d3b65330e757
martagrio <marta.igr90@gmail.com>
2019-08-08 09:42:24 +0200

    Add variables in js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,7 +2,8 @@
 
 const urlBase = 'http://api.tvmaze.com/search/shows?q=';
 
-const name = 'girls';
+const name = document.querySelector('.input--serie').value;
+console.log(name);
 const endpoint = urlBase + name;
 
 console.log(endpoint);
