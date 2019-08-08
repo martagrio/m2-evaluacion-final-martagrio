02c44cd82b039e85545872bfaac0853495d78449
martagrio <marta.igr90@gmail.com>
2019-08-08 11:01:11 +0200

    Upate function to add the series with the searched name

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const urlBase = 'http://api.tvmaze.com/search/shows?q=';
-
+const defaultImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 const name = document.querySelector('.input--serie').value;
 const list = document.querySelector('.series__list');
-const image = document.querySelector('.serie__img');
-const title = document.querySelector('.serie__title');
 
 const endpoint = urlBase + name;
 
@@ -14,19 +12,21 @@ fetch(endpoint)
   .then(data => getShow(data));
 
 function getShow (array){
-  let listContent = '';
+
   for (let i = 0; i < array.length; i++) {
-    title.innerHTML = array[i].show.name;
-    image.src = array[i].show.image.original;
+    const show = document.createElement('li');
+    const image = document.createElement('img');
+    const title = document.createElement('h2');
+    const titleContent = document.createTextNode(array[i].show.name);
+    image.src = array[i].show.image.original || array[i].show.image.medium || defaultImage;
     image.alt = array[i].show.name;
+    title.appendChild(titleContent);
+    show.appendChild(title);
+    show.appendChild(image);
+    list.appendChild(show);
   }
-  list.appendChild();
+
 }
 
 
-const show = document.createElement('li');
-const image = document.createElement('img');
-const title = document.createElement('h2');
-const titleContent = document.createTextNode('');
-show.appendChild(image);
-show.appendChild(title);
+
